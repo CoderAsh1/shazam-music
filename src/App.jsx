5b7c27b820cb25a3e4bcf216de991b824c12d3c2
coderAsh1 <ashutoshsasmal1@gmail.com>
2022-12-02 20:31:05 +0530

    store setup done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { useGetTopChatsQuery } from "./store/slices/shazamApi";
 
 function App() {
+  const { data, isLoading } = useGetTopChatsQuery();
+  console.log(data);
+
   return (
     <div className="App">
       <h1 className="text-center text-green-700 font-bold">Shazam</h1>
